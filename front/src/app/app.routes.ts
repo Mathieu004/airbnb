10ba1d9e0d13914bb58e4aacb3b3ed0b8810daf6
Mathieu004 <matcaz33.mobile@gmail.com>
2026-03-27 11:00:56 +0100

    angular init

---
+++ front/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { PageHomeComponent } from '../page-home/page-home.component';
+
+export const routes: Routes = [
+  { path: '', component: PageHomeComponent }
+];
